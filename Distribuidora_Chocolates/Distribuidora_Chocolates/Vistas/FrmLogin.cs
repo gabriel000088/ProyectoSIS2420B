7c31c917a20623e3d60f40349edfb82847513142
gabriel000088 <ivanf6214@gmail.com>
2023-06-29 21:11:13 -0400

    Diseño orignal aplicado al loginde login aplicado (iconos)

--- Distribuidora_Chocolates/Distribuidora_Chocolates/Vistas/FrmLogin.cs
+++ Distribuidora_Chocolates/Distribuidora_Chocolates/Vistas/FrmLogin.cs
@@ -58,5 +58,10 @@ namespace Distribuidora_Chocolates
         {
 
         }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
--- Distribuidora_Chocolates/Distribuidora_Chocolates/Vistas/FrmMenuPrincipal.Designer.cs
+++ Distribuidora_Chocolates/Distribuidora_Chocolates/Vistas/FrmMenuPrincipal.Designer.cs
@@ -28,12 +28,82 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.gestionarProductosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.gestionarClientesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.gestionarUsuariosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.resgistrarVentasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.generarReportesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.gestionarProductosToolStripMenuItem,
+            this.gestionarClientesToolStripMenuItem,
+            this.gestionarUsuariosToolStripMenuItem,
+            this.resgistrarVentasToolStripMenuItem,
+            this.generarReportesToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // gestionarProductosToolStripMenuItem
+            // 
+            this.gestionarProductosToolStripMenuItem.Name = "gestionarProductosToolStripMenuItem";
+            this.gestionarProductosToolStripMenuItem.Size = new System.Drawing.Size(126, 20);
+            this.gestionarProductosToolStripMenuItem.Text = "Gestionar Productos";
+            // 
+            // gestionarClientesToolStripMenuItem
+            // 
+            this.gestionarClientesToolStripMenuItem.Name = "gestionarClientesToolStripMenuItem";
+            this.gestionarClientesToolStripMenuItem.Size = new System.Drawing.Size(114, 20);
+            this.gestionarClientesToolStripMenuItem.Text = "Gestionar Clientes";
+            // 
+            // gestionarUsuariosToolStripMenuItem
+            // 
+            this.gestionarUsuariosToolStripMenuItem.Name = "gestionarUsuariosToolStripMenuItem";
+            this.gestionarUsuariosToolStripMenuItem.Size = new System.Drawing.Size(117, 20);
+            this.gestionarUsuariosToolStripMenuItem.Text = "Gestionar Usuarios";
+            // 
+            // resgistrarVentasToolStripMenuItem
+            // 
+            this.resgistrarVentasToolStripMenuItem.Name = "resgistrarVentasToolStripMenuItem";
+            this.resgistrarVentasToolStripMenuItem.Size = new System.Drawing.Size(107, 20);
+            this.resgistrarVentasToolStripMenuItem.Text = "Resgistrar Ventas";
+            // 
+            // generarReportesToolStripMenuItem
+            // 
+            this.generarReportesToolStripMenuItem.Name = "generarReportesToolStripMenuItem";
+            this.generarReportesToolStripMenuItem.Size = new System.Drawing.Size(109, 20);
+            this.generarReportesToolStripMenuItem.Text = "Generar Reportes";
+            // 
+            // FrmMenuPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "FrmMenuPrincipal";
             this.Text = "FrmMenuPrincipal";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem gestionarProductosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem gestionarClientesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem gestionarUsuariosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem resgistrarVentasToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem generarReportesToolStripMenuItem;
     }
 }
